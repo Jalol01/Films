e3d8a99bb70e8bf7c64326cf07a28f1093677a6b
Jalol01 <82259478+Jalol01@users.noreply.github.com>
2023-03-09 18:42:23 +0500

    first commit

---
+++ src/components/app-info/AppInfo.vue
@@ -0,0 +1,26 @@
+<template>
+  <Box class="app-info">
+    <p class="fs-3 text-upparcase">Barcha kinolar soni: {{allMoviesCount}}</p>
+    <p class="fs-4 text-upparcase">Ko`rilgan kinolar soni: {{favouriteMoviesCount}}</p>
+  </Box>
+</template>
+
+<script>
+export default {
+    props:{
+      allMoviesCount:{
+        type: Number,
+        required: true
+      },
+      favouriteMoviesCount:{
+        type: Number,
+        required: true,
+      }
+    }
+}
+</script>
+
+<style scoped>
+
+
+</style>
---
+++ src/components/app/App.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="app font-monospace">
+    <div class="content">
+      <AppInfo
+        :allMoviesCount="movies.length"
+        :favouriteMoviesCount="movies.filter((c) => c.favourite).length"
+      />
+      <div class="search-panel">
+        <SearchPanel :updateTermHandler="updateTermHandler" />
+        <AppFilter :filterName="filter" :updateFilterHandler="updateFilterHandler" />
+      </div>
+      <Movie-list
+        :movies="onFilterHandler(onSearchHandler(movies, term), filter)"
+        @onToggle="onToggleHandler"
+        @onRemove="onRemoveHandler"
+      />
+      <MovieAddForm @createMovie="createMovie" />
+    </div>
+  </div>
+</template>
+
+<script>
+import AppFilter from "../app-filter/AppFilter.vue";
+import MovieAddForm from "../movie-add--form/MovieAddForm.vue";
+import MovieList from "../movie-list/MovieList.vue";
+import AppInfo from "../app-info/AppInfo.vue";
+import SearchPanel from "../search-panel/SearchPanel.vue";
+export default {
+  components: {
+    AppInfo,
+    SearchPanel,
+    AppFilter,
+    MovieList,
+    MovieAddForm,
+  },
+  data() {
+    return {
+      movies: [
+        {
+          name: "Omar",
+          viewers: "811",
+          favourite: false,
+          like: true,
+          id: 1,
+        },
+        {
+          name: "Empire of Osman",
+          viewers: "411",
+          favourite: false,
+          like: false,
+          id: 2,
+        },
+        {
+          name: "Ertugrul",
+          viewers: "711",
+          favourite: true,
+          like: false,
+          id: 3,
+        },
+      ],
+      term: "",
+      filter: "all",
+    };
+  },
+  methods: {
+    createMovie(item) {
+      this.movies.push(item);
+      console.log(item);
+    },
+    onToggleHandler({ id, prop }) {
+      this.movies = this.movies.map((item) => {
+        if (item.id == id) {
+          return { ...item, [prop]: !item[prop] };
+        }
+        return item;
+      });
+    },
+    onRemoveHandler(id) {
+      this.movies = this.movies.filter((c) => c.id != id);
+    },
+    onSearchHandler(arr, term) {
+      if (term.length == 0) {
+        return arr;
+      }
+      return arr.filter((c) => c.name.toLowerCase().indexOf(term) > -1);
+    },
+    onFilterHandler(arr, filter) {
+      switch (filter) {
+        case "popular":
+          return arr.filter((c) => c.like);
+        case "mostViewers":
+          return arr.filter((c) => c.viewers > 500);
+        default:
+          return arr
+      }
+    },
+    updateTermHandler(term) {
+      this.term = term;
+    },
+    updateFilterHandler(filter){
+      this.filter = filter
+    }
+  },
+};
+</script>
+
+<style>
+.app {
+  height: 100vh;
+  color: #000;
+}
+.content {
+  width: 1000px;
+  min-height: 700px;
+  background-color: #fff;
+  margin: 0 auto;
+  padding: 5rem 0;
+}
+.search-panel {
+  margin-top: 2rem;
+  padding: 1.5rem;
+  background-color: #fcfaf5;
+  border-radius: 4px;
+  box-shadow: 15px 15px 15px rgba(0, 0, 0, 0.15);
+}
+</style>
